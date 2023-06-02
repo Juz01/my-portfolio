c86521e2383a67b2538f8f80f4368a62469012df
Juz01 <jorgeuz01@hotmail.com>
2023-06-01 20:07:08 -0400

    Portfolio. First Commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+
+import './App.css'
+
+const App = () => {
+  
+
+  return (
+    <div className='container'>
+
+        {/* home */}
+        <div className='nav__container'>
+          <nav className='navbar'>
+            <div className='nav__logo'>
+              <a href="#home" id='home'>
+                <img src="./logo.jpeg" className='logo' alt="logo" />
+              </a>
+            </div>
+            
+            <ul className='nav__item'>
+              <a href="#about-me">
+                <li className='items'>About Me</li>
+              </a>
+              <a href="#skills">
+                <li className='items'>Skills</li>
+              </a>
+              <a href="#contacte-me">
+                <li className='items'>Contact Me</li>
+              </a>
+            </ul>
+          </nav>
+        </div>
+      
+
+        {/* About me */}
+        <div className='about-me__container'>
+          <div className='about-me' id='about-me'>
+            <div className='box'></div>
+            <div>
+              <img src="./public/presentation.jpeg" className='presentation__img' alt="presentation" />
+            </div>
+            <div className='about-me'>
+              <h1 className='about-me__info'>Jorge Luis Uceta Familia</h1>
+              <p className='about-me__body'>
+                Soy un joven de <span>Republica Dominicana</span>, que como una persona proactiva me gusta esta siempre <span>aprender</span> cosas nuevas, <span>sin miedo</span> a equivocarme y con todo el animo de poder <span>superar</span> cualquier barrera. <br />
+                Actualmente estudiando en <span className='academlo'>Academlo</span>, en el cual junto con los demas compañeros hemos realizados varios <span>proyectos</span>, con el motivo de poder llegar a ser un <span>Senior Developer</span>. <br />
+                Como hobbie me gusta pasar tiempo con mi familia, escuchar musica, ejercicios y bueno pues programar tambien, estas son pequeños detalles sobre mi.
+              </p>
+              </div>
+          </div>
+        </div>
+      
+
+        {/* Skills */}
+      <section className='skills' id='skills'>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </section>
+      
+        {/* Contact Me */}
+      <footer className='contacte-me' id='contacte-me'>
+        WS, IG, TW, Email, Linkedin
+        <a href="#home">Go up</a>
+      </footer>
+      
+    </div>
+  )
+}
+
+export default App
