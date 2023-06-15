07f8468fd713443f1c34df1949eb47f976b02ee8
Juz01 <jorgeuz01@hotmail.com>
2023-06-14 21:01:54 -0400

    modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,14 @@
 
 import './App.css'
+import NavBar from './components/NavBar'
+
 
 const App = () => {
   
 
   return (
-    <div className='container'>
-
-        {/* home */}
-        <div className='nav__container'>
-          <nav className='navbar'>
-            <div className='nav__logo'>
-              <a href="#home" id='home'>
-                <img src="./logo.jpeg" className='logo' alt="logo" />
-              </a>
-            {/* <div className='box'></div> */}
-            </div>
-            
-            <ul className='nav__item'>
-              <a href="#about-me">
-                <li className='items'>About Me</li>
-              </a>
-              <a href="#skills">
-                <li className='items'>Skills</li>
-              </a>
-              <a href="#contacte-me">
-                <li className='items'>Contact Me</li>
-              </a>
-            </ul>
-          </nav>
-        </div>
-      
-
-        {/* About me */}
-        <div className='about-me__container'>
-          <div className='about-me' id='about-me'>
-            <div>
-              <img src="./presentation.jpeg" className='presentation__img' alt="presentation" />
-            </div>
-            <div className='about-me__presentation'>
-              <h1 className='about-me__info'>I'm Jorge Luis Uceta. A Full Stack Developer <span>From Dominican Rep.</span></h1>
-              <p className='about-me__body'>
-                Soy un joven de <span>Republica Dominicana</span>, que como una persona proactiva me gusta esta siempre <span>aprender</span> cosas nuevas, <span>sin miedo</span> a equivocarme y con todo el animo de poder <span>superar</span> cualquier barrera. <br />
-                Actualmente estudiando en <span className='academlo'>Academlo</span>, en el cual junto con los demas compañeros hemos realizados varios <span>proyectos</span>, con el motivo de poder llegar a ser un <span>Senior Developer</span>. <br />
-                Como hobbie me gusta pasar tiempo con mi familia, escuchar musica, ejercicios y bueno pues programar tambien, estas son pequeños detalles sobre mi.
-              </p>
-              </div>
-          </div>
-        </div>
-      
-
-        {/* Skills */}
-      <section className='skills' id='skills'>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-      </section>
-      
-        {/* Contact Me */}
-      <footer className='contacte-me' id='contacte-me'>
-        WS, IG, TW, Email, Linkedin
-        <a href="#home">Go up</a>
-      </footer>
-      
+    <div className='app'>
+      <NavBar />
     </div>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import navbar from './styles/navbar.styles.css'
+
+const NavBar = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
+  return (
+    // This is the portfolio's Navbar
+    <div className='nav__container'>
+        <nav className='nav__nav'>
+            <div className='nav__header'>
+            {/* Home Logo */}
+            <a href="#home" className='nav__logo'>
+              <img src="logo.jpeg" className='nav__img' alt="logo" />
+            </a>
+
+            <div className='nav__menu'>
+                {/* Menu Icon */}
+                <div className='nav__menu-icon'>
+                    <i onClick={toggleMenu} className='bx bx-menu-alt-right'></i>
+                </div>
+
+                {/* NavBar options */}
+                <ul className={`nav__list ${isOpen ? "is-open" : ""}`}>
+                    <li className='nav__item'>
+                        <a href="#about-me">About me</a>
+                    </li>
+                    <li className='nav__item'>
+                        <a href="#skills">Skills</a>
+                    </li>
+                    <li className='nav__item'>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li className='nav__item'>
+                        <a href="#contact-me">Contact me</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+
+            {/* Picture's presentation */}
+            <div className='presentation__container'>
+                <img src="j-u-z.jpeg" className='presentation-me' alt="me" />
+            </div>
+
+            {/* Presentation's Info */}
+            <h1 className='nav__title'>
+                I'm Jorge L. Uceta. A Junior Full Stack Dev. <span>From Dominican Rep.</span>
+            </h1>
+            <p className='nav__body'>
+                Be sure I'm a person who wants alway learn new things and if you are trying to reach out a hard worker. <span>I'm your guy</span>.
+            </p>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
